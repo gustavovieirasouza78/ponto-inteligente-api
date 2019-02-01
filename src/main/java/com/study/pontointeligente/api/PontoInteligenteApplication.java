9390bf8f3136c3509c68df49f2f1b7aa954c9619
Gustavo Souza <gustavovieirasouza78@gmail.com>
2019-02-01 16:09:38 +0100

    EhCache

--- src/main/java/com/study/pontointeligente/api/PontoInteligenteApplication.java
+++ src/main/java/com/study/pontointeligente/api/PontoInteligenteApplication.java
@@ -3,11 +3,13 @@ package com.study.pontointeligente.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 
 //@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @SpringBootApplication()
 @ComponentScan("com.study.pontointeligente.api")
+@EnableCaching
 public class PontoInteligenteApplication {
 
 	public static void main(String[] args) {
